58b472089582eb1bd03d3e1ea97526bcf5fae37c
Uziel <201274@ids.upchiapas.edu.mx>
2021-04-23 12:04:27 -0500

    terminado

--- Veterinaria.sql
+++ Veterinaria.sql
@@ -2,23 +2,21 @@ CREATE SCHEMA `veterinaria` ;
 
 CREATE TABLE `animales` (
   `IdAnimales` int NOT NULL AUTO_INCREMENT,
-  `idClientes` int NOT NULL,
+  `NombreCliente` varchar(50) NOT NULL,
   `Nombre` varchar(50) NOT NULL,
   `Color` varchar(50) NOT NULL,
   `Especie` varchar(50) NOT NULL,
   `Raza` varchar(45) DEFAULT NULL,
   `Peso` int DEFAULT NULL,
-  `Ingreso` varchar(20) default null,
-  `Motivo` varchar(50) default null,
+  `Motivo` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`IdAnimales`)
 );
 
 CREATE TABLE `citas` (
-  `IdCitas` int NOT NULL auto_increment,
+  `IdCitas` int NOT NULL AUTO_INCREMENT,
   `IdAnimales` varchar(45) DEFAULT NULL,
-  `Fecha` varchar(20) default NULL,
-  `Hora` varchar (20) default null,
-  `Motivo` varchar (50) default null,
+  `Fecha` varchar(20) DEFAULT NULL,
+  `Hora` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`IdCitas`)
 );
 
@@ -32,14 +30,24 @@ CREATE TABLE `clientes` (
 );
 
 CREATE TABLE `medicamentos` (
-  `IdMedicamento` int auto_increment NOT NULL,
-  `Codigo` int default null,
+  `IdMedicamento` int NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(50) DEFAULT NULL,
   `Precio` int DEFAULT NULL,
   `Cantidad` int DEFAULT NULL,
   `fechaCaducidad` varchar(15) DEFAULT NULL,
   `sustanciaActiva` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`IdMedicamento`)
+);
+
+CREATE TABLE `registrados` (
+  `idRegistrados` int NOT NULL AUTO_INCREMENT,
+  `NombreCliente` varchar(45) DEFAULT NULL,
+  `NombreAnimal` varchar(45) DEFAULT NULL,
+  `Color` varchar(45) DEFAULT NULL,
+  `Especie` varchar(45) DEFAULT NULL,
+  `Raza` varchar(45) DEFAULT NULL,
+  `Motivo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idRegistrados`)
 );
 
 CREATE TABLE `usuario` (
